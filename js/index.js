c3206871fbad5f71ddfeb1f8d2e05fd6b2d6d4c6
garyyufei <garyyufei@gmail.com>
2016-12-09 18:04:44 +1100

    add new features

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-	$('.timelineitem').mouseenter(function(){
-		$(this).addClass('change');
-	});
-
-	$('.timelineitem').mouseleave(function(){
-		$(this).removeClass('change');
-	});
     
     $('.nailthumb-container').nailthumb({width:180,height:170});
+    $('.nailthumb-experiment-container').nailthumb({width:100,height:85});
+    $('.nailthumb-paper-container').nailthumb({width:320,height:240});
+    
+    $('#undergarduate_node').click(function(){
+    	$('.undergarduate').toggle("slow");
+    	if($('.undergarduate').is(":visible")) {
+    		$('.undergarduate').removeAttr('style');
+    	}
+    });
+
+    $('.undergarduate').hide();
 });
