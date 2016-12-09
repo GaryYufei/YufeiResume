656e1506a1e353311cc9b79e7ff07e91a77e16f1
garyyufei <garyyufei@gmail.com>
2016-12-09 18:10:16 +1100

    add new features

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
     $('.nailthumb-container').nailthumb({width:180,height:170});
     $('.nailthumb-experiment-container').nailthumb({width:100,height:85});
-    $('.nailthumb-paper-container').nailthumb({width:320,height:240});
+    $('.nailthumb-paper-container').nailthumb({width:280,height:200});
     
     $('#undergarduate_node').click(function(){
     	$('.undergarduate').toggle("slow");
